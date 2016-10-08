437225af9c91f77efef49cec915e4136fbfdc894
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-10-08 17:38:55 +0100

    Some refactoring

--- print_diamond_take_three/lib/diamond.rb
+++ print_diamond_take_three/lib/diamond.rb
@@ -2,41 +2,47 @@ class Diamond
 
   FIRST_LETTER = 'A'
 
-  def self.full_diamond(chars)
-    return ''  if chars == 0
-    return 'A' if chars == 1
+  def initialize(total_characters)
+    @line_width = @total_rows = calculate_total_rows(total_characters)
+  end
 
+  def full_diamond
     full_diamond = ''
-    line_width = total_rows = (chars * 2) - 1
 
     total_rows.times do |row|
-      index  = row_to_index(total_rows, row)
-      letter = find_letter(FIRST_LETTER, index)
-      full_diamond << line(line_width, index, letter) + "\n"
+      offset = character_offset_from_centre_of_line(total_rows, row)
+      letter = find_letter(FIRST_LETTER, offset)
+      full_diamond << compose_line(line_width, offset, letter) + "\n"
     end
     
     full_diamond.chomp
   end
 
-  def self.line(width, index, symbol)
-    outer_space = ' ' * (((width - 1) / 2) - index) 
+  private
 
-    if index == 0
-      return outer_space + symbol + outer_space 
+  def compose_line(width, offset_from_centre, character)
+    outer_space = ' ' * (((width - 1) / 2) - offset_from_centre) 
+
+    if offset_from_centre == 0
+      return outer_space + character + outer_space 
     else
       inner_space = ' ' * (width - (2 * outer_space.size) - 2)
-      return outer_space + symbol + inner_space + symbol +  outer_space
+      return outer_space + character + inner_space + character +  outer_space
     end
   end
 
-  private
+  attr_reader :line_width, :total_rows
 
-  def self.row_to_index(total_rows, row)
+  def calculate_total_rows(total_characters)
+    (total_characters * 2) - 1
+  end
+
+  def character_offset_from_centre_of_line(total_rows, row)
     return total_rows - row - 1 if row > (total_rows - 1)/2
     row
   end
 
-  def self.find_letter(start, offset)
+  def find_letter(start, offset)
     (start.ord + offset).chr
   end
 
--- print_diamond_take_three/spec/diamond_spec.rb
+++ print_diamond_take_three/spec/diamond_spec.rb
@@ -2,48 +2,22 @@ require 'diamond'
 
 describe Diamond, ':full_diamond' do
 
+  let(:test_diamond) { described_class }
+
   it 'returns an empty string for 0' do
-    expect(Diamond.full_diamond(0)).to eq ''
+    expect(test_diamond.new(0).full_diamond).to eq ''
   end 
 
   it 'returns an A for 1' do
-    expect(Diamond.full_diamond(1)).to eq 'A'
+    expect(test_diamond.new(1).full_diamond).to eq 'A'
   end 
 
   it 'returns appropriate diamond for 2' do
-    expect(Diamond.full_diamond(2)).to eq " A \nB B\n A "
+    expect(test_diamond.new(2).full_diamond).to eq " A \nB B\n A "
   end 
 
   it 'returns appropriate diamond for 3' do
-    expect(Diamond.full_diamond(3)).to eq "  A  \n B B \nC   C\n B B \n  A  "
-  end 
-
-end
-
-describe Diamond, ':line' do
-
-  it 'returns the appropriate string for width 1, index 0' do
-    expect(Diamond.line(1, 0, 'x')).to eq 'x'
-  end 
-
-  it 'returns the appropriate string for width 3, index 0' do
-    expect(Diamond.line(3, 0, 'x')).to eq ' x '
-  end 
-
-  it 'returns the appropriate string for width 3, index 1' do
-    expect(Diamond.line(3, 1, 'x')).to eq 'x x'
-  end 
-
-  it 'returns the appropriate string for width 5, index 0' do
-    expect(Diamond.line(5, 0, 'x')).to eq '  x  '
-  end 
-
-  it 'returns the appropriate string for width 5, index 1' do
-    expect(Diamond.line(5, 1, 'x')).to eq ' x x '
-  end 
-
-  it 'returns the appropriate string for width 5, index 2' do
-    expect(Diamond.line(5, 2, 'x')).to eq 'x   x'
+    expect(test_diamond.new(3).full_diamond).to eq "  A  \n B B \nC   C\n B B \n  A  "
   end 
 
 end
